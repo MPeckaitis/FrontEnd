c342c7505fdf341628131c2ff1de99e00e55d46a
Martynas Pečkaitis <peckaitis.martynas@gmail.com>
2017-03-16 21:18:38 +0200

    added  js to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 gulp.task('connect', function() {
     connect.server({
@@ -18,9 +19,16 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('./web/assets'));
 });
 
+gulp.task('js', function() {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./web/assets'));
+});
+
 gulp.task('watch', function() {
     gulp.watch(['./web/*.html'], ['html']);
     gulp.watch('./src/sass/**/*.scss', ['sass']);
+    gulp.watch('./src/js/**/*.js', ['js']);
 });
 
-gulp.task('default', ['connect', 'sass', 'watch']);
+gulp.task('default', ['connect', 'sass', 'js', 'watch']);
